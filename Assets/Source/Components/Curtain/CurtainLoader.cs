21070f6bd1b6c69b65bf8cfada0c313d28840788
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2025-03-22 01:48:32 +0400

    Changed IDE

--- Assets/Source/Components/Curtain/CurtainLoader.cs
+++ Assets/Source/Components/Curtain/CurtainLoader.cs
@@ -1,25 +1,26 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Source.Components.Curtain
 {
     public class CurtainLoader : MonoBehaviour
     {
-        [SerializeField, Min(0)] private float _duration;
+        [FormerlySerializedAs("_duration")] [SerializeField, Min(0)] private float duration;
 
-        [SerializeField] private Image _panel;
+        [FormerlySerializedAs("_panel")] [SerializeField] private Image panel;
 
         public void Hide()
         {
-            if (_panel.color.a == 0f)
+            if (panel.color.a == 0f)
                 return;
 
-            _panel.DOFade(0f, _duration)
+            panel.DOFade(0f, duration)
                 .SetEase(Ease.Linear);
         }
 
         public void Show() =>
-            _panel.color = Color.black;
+            panel.color = Color.black;
     }
 }
